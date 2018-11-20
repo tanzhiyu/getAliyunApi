1b72fe087e8bb62fa539b1735e61801fdf30255c
tanzhiyu <615784818@qq.com>
2018-11-20 14:09:30 +0800

    change

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-const info = '我是从server分支checkout过来的'
+console.log(module.paths)
+console.log(__dirname)
+console.log(require.extensions['.js'].toString())
+console.log(__filename)
+
+
+var change = a => {
+  console.log(a ===b )
+  a = [7]
+  console.log(a)
+}
+var a = {a: 'a'}
+var b = a
+change(a)
+console.log(a)
+
+console.log(b)
